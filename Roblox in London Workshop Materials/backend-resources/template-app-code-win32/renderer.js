05970fbc0447e803b023052a0fdc88dbc48964d6
Kaitlyn Hornbuckle <124624417+hornbuck@users.noreply.github.com>
2025-03-19 01:04:23 -0700

    Update renderer.js

--- Roblox in London Workshop Materials/backend-resources/template-app-code-win32/renderer.js
+++ Roblox in London Workshop Materials/backend-resources/template-app-code-win32/renderer.js
@@ -2,7 +2,7 @@ const { ipcRenderer } = require('electron');
 
 document.getElementById('imageButton').addEventListener('click', async () => {
     
-    const fileUrl = 'https://raw.githubusercontent.com/cncf-kids-day/workshops/main/Roblox-in-London.rbxl'; // Replace with your file URL
+    const fileUrl = 'https://raw.githubusercontent.com/cncf-kids-day/workshops/main/Roblox-in-London.rbxl'; // Pulls latest Roblox project
     
     // Send the download request to the main process
     ipcRenderer.send('download-file', { url: fileUrl });
